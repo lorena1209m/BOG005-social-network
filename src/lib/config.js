0892bf76ac1dfbac00b1612e45a5278e4b832ece
Lorena Moreno <kmoreno1209@gmail.com>
2022-10-21 12:02:25 -0500

    cambios en gitignore

---
+++ src/lib/config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: 'AIzaSyD_j6708J42cmSLKkysThgLNEPr0F8W9cY',
+    authDomain: 'social-network-d92e0.firebaseapp.com',
+    projectId: 'social-network-d92e0',
+    storageBucket: 'social-network-d92e0.appspot.com',
+    messagingSenderId: '551834649107',
+    appId: '1:551834649107:web:9ad2286e4f3d8fdc5775e6',
+    measurementId: 'G-SBGE4HSXQM',
+  };
+  
